97d160a403241ccb68953b010a5afe256d63088d
Buma Kazuya <laughing00boomer@gmail.com>
2023-08-04 19:41:04 +0900

    update fresh-app03

--- fresh-app03/islands/Container.tsx
+++ fresh-app03/islands/Container.tsx
@@ -3,6 +3,25 @@ function script() {
   const canvas = document.getElementById('canvas1') as HTMLCanvasElement;
   const ctx = canvas.getContext("2d");
   console.log(ctx);
+
+  const CANVAS_WIDTH = canvas.width = 600;
+  const CANVAS_HEIGHT = canvas.height = 600;
+
+  const playerImage = new Image();
+  playerImage.src = 'shadow_dog.png'
+  const sprintWidth = 575;
+  const sprintHeight = 523;
+
+  function animate() {
+    if (ctx != null) {
+      ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+      // ctx.fillRect(100, 50, 100, 100);
+      ctx.drawImage(playerImage, 0, 0, sprintWidth, sprintHeight, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+      requestAnimationFrame(animate);
+    }
+  }
+
+  animate();
 }
 
 export function Container() {
