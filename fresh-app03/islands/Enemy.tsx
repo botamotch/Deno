fbc9c1953d2ed6c50f2a173308ef9ecfb59aa836
Kazuya Buma <laughing00boomer@gmail.com>
2023-08-11 22:51:41 +0900

    update fresh-app03 - enemy movement
    1:58:15

--- fresh-app03/islands/Enemy.tsx
+++ fresh-app03/islands/Enemy.tsx
@@ -13,29 +13,63 @@ function script() {
   const ctx = canvas.getContext("2d");
   const CANVAS_WIDTH = canvas.width = 500;
   const CANVAS_HEIGHT = canvas.height = 1000;
-  const numberOfEnemies = 100;
+  const numberOfEnemies = 10;
   const enemiesArray: Enemy[] = [];
+
+  let gameFrame = 0;
 
   class Enemy {
     x: number;
     y: number;
     width: number;
     height: number;
+    spriteWidth: number;
+    spriteHeight: number;
     speed: number;
+    frame: number;
+    flapSpeed: number;
+    image: HTMLImageElement;
+    angle: number;
+    angleSpeed: number;
+    curve: number;
     constructor() {
-      this.x = Math.random() * canvas.width;
-      this.y = Math.random() * canvas.width;
-      this.width = 100;
-      this.height = 100;
-      this.speed = Math.random() * 4 -2;
+      this.image = new Image();
+      this.image.src = "enemy2.png";
+      this.spriteWidth = 266;
+      this.spriteHeight = 188;
+      this.width = this.spriteWidth / 2.5;
+      this.height = this.spriteHeight / 2.5;
+      this.x = Math.random() * (canvas.width - this.width);
+      this.y = Math.random() * (canvas.height - this.height);
+      this.speed = Math.random() * 4 + 1;
+      this.frame = 0;
+      this.flapSpeed = Math.floor(Math.random() * 3 + 1);
+      this.angle = Math.random() * 2;
+      this.angleSpeed = Math.random() * 0.2;
+      this.curve = Math.random() * 10;
     }
     update() {
-      this.x += this.speed;
-      this.y += this.speed;
+      this.x -= this.speed;
+      this.y += this.curve * Math.sin(this.angle);
+      this.angle += this.angleSpeed;
+      if (this.x + this.width < 0) this.x = canvas.width;
+      if (gameFrame % this.flapSpeed === 0) {
+        this.frame > 4 ? this.frame = 0 : this.frame++;
+      }
     }
     draw() {
       assertIsDefined(ctx);
-      ctx.fillRect(this.x, this.y, this.width, this.height);
+      ctx.drawImage(
+        this.image,
+        this.frame * this.spriteWidth,
+        0,
+        this.spriteWidth,
+        this.spriteHeight,
+        this.x,
+        this.y,
+        this.width,
+        this.height,
+      );
     }
   }
 
@@ -51,6 +85,7 @@ function script() {
       enemy.update();
       enemy.draw();
     });
+    gameFrame++;
 
     requestAnimationFrame(animate);
   }
