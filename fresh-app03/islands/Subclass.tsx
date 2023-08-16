3464c9e746f36a9e1385d24896451c79e1a94650
Buma Kazuya <laughing00boomer@gmail.com>
2023-08-16 10:37:30 +0900

    update fresh-app03

--- fresh-app03/islands/Subclass.tsx
+++ fresh-app03/islands/Subclass.tsx
@@ -16,32 +16,86 @@ function script() {
 
     canvas.width = 500;
     canvas.height = 800;
-    let lastTime = 1;
 
     class Game {
       enemies: Enemy[];
-      constructor() {
+      ctx: CanvasRenderingContext2D;
+      width: number;
+      height: number;
+      enemyInterval: number;
+      enemyTimer: number;
+      constructor(
+        ctx: CanvasRenderingContext2D,
+        width: number,
+        height: number,
+      ) {
+        this.ctx = ctx;
+        this.width = width;
+        this.height = height;
         this.enemies = [];
+        this.enemyInterval = 1000;
+        this.enemyTimer = 0;
+        this.#addEnewEnemy();
       }
 
-      update() {}
-      draw() {}
-      #addEnewEnemy() {}
+      update(deltaTime: number) {
+        this.enemies = this.enemies.filter((object) =>
+          !object.markedForDeletion
+        );
+        if (this.enemyTimer > this.enemyInterval) {
+          this.#addEnewEnemy();
+          this.enemyTimer = 0;
+        } else {
+          this.enemyTimer += deltaTime;
+        }
+        this.enemies.forEach((object) => object.update());
+      }
+      draw() {
+        this.enemies.forEach((object) => object.draw(this.ctx));
+      }
+      #addEnewEnemy() {
+        this.enemies.push(new Enemy(this));
+      }
     }
 
     class Enemy {
-      constructor() {}
-      update() {}
-      draw() {}
+      x: number;
+      y: number;
+      width: number;
+      height: number;
+      game: Game;
+      markedForDeletion: boolean;
+      constructor(game: Game) {
+        this.game = game;
+        this.x = this.game.width;
+        this.y = Math.random() * this.game.height;
+        this.width = 100;
+        this.height = 100;
+        this.markedForDeletion = false;
+      }
+      update() {
+        this.x--;
+        if (this.x < 0 - this.width) {
+          this.markedForDeletion = true;
+        }
+      }
+      draw(ctx: CanvasRenderingContext2D) {
+        assertIsDefined(ctx);
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+      }
     }
 
+    const game = new Game(ctx, canvas.width, canvas.height);
+
+    let lastTime = Date.now();
     function animate() {
       assertIsDefined(ctx);
       ctx.clearRect(0, 0, canvas.width, canvas.height);
-      const timeStamp = Date.now();
-      const deltaTime = timeStamp - lastTime;
-      lastTime = timeStamp;
-      // console.log(deltaTime);
+      const presentTime = Date.now();
+      const deltaTime = presentTime - lastTime;
+      lastTime = presentTime;
+      game.update(deltaTime);
+      game.draw();
       requestAnimationFrame(animate);
     }
 
