7f3cbbea7d8116593c33d2f79273d8772f92bf9b
Buma Kazuya <laughing00boomer@gmail.com>
2023-08-17 17:19:36 +0900

    update docker-test

--- docker-test/routes/index.tsx
+++ docker-test/routes/index.tsx
@@ -18,7 +18,7 @@ export default function Home() {
             height="128"
             alt="the fresh logo: a sliced lemon dripping with juice"
           />
-          <h1 class="text-4xl font-bold">Welcome to fresh !!!</h1>
+          <h1 class="text-4xl font-bold">Welcome to fresh</h1>
           <p class="my-4">
             Try updating this message in the
             <code class="mx-2">./routes/index.tsx</code> file, and refresh.
