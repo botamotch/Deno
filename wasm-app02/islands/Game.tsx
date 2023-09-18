4763ff1ce61467719727e5fd83424142879fb76b
Kazuya Buma <botamotch.monster@gmail.com>
2023-09-18 17:11:44 +0900

    update wasm-app02 - Button

--- wasm-app02/islands/Game.tsx
+++ wasm-app02/islands/Game.tsx
@@ -17,9 +17,9 @@ export default function Game() {
       <button
         type="button"
         class="rounded-lg border border-gray-300 bg-white px-5 py-2.5 text-center text-sm font-medium text-gray-700 shadow-sm transition-all hover:bg-gray-100 focus:ring focus:ring-gray-100 disabled:cursor-not-allowed disabled:border-gray-100 disabled:bg-gray-50 disabled:text-gray-400 my-4"
-        onClick={() => main()}
+        onClick={() => console.log(Date.now().toString())}
       >
-        Reload
+        Button
       </button>
       <div class="flex justify-center">
         <canvas
