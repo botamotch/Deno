ac52b0293554142d480cebd66888825140684aab
Kazuya Buma <botamotch.monster@gmail.com>
2023-10-06 16:47:56 +0900

    update wasm-app02 - delete old code

--- wasm-app02/rs_lib/src/game.rs
+++ wasm-app02/rs_lib/src/game.rs
@@ -1,7 +1,6 @@
 use crate::browser;
 use crate::engine;
 use crate::engine::KeyState;
-use crate::engine::Point;
 use crate::engine::{Game, Rect, Renderer};
 use anyhow::anyhow;
 use anyhow::Result;
@@ -29,22 +28,12 @@ pub struct Sheet {
 }
 
 pub struct WalkTheDog {
-  // image: Option<HtmlImageElement>,
-  // sheet: Option<Sheet>,
-  frame: u8,
-  position: Point,
   rhb: Option<red_hat_boy::RedHatBoy>,
 }
 
 impl WalkTheDog {
   pub fn new() -> Self {
-    WalkTheDog {
-      // image: None,
-      // sheet: None,
-      frame: 0,
-      position: Point { x: 0, y: 0 },
-      rhb: None,
-    }
+    WalkTheDog { rhb: None }
   }
 }
 
@@ -57,10 +46,6 @@ impl Game for WalkTheDog {
     let image = Some(engine::load_image("rhb.png").await?);
 
     Ok(Box::new(WalkTheDog {
-      // image: image.clone(),
-      // sheet: sheet.clone(),
-      frame: self.frame,
-      position: self.position,
       rhb: Some(red_hat_boy::RedHatBoy::new(
         sheet.clone().ok_or_else(|| anyhow!("No Sheet Present"))?,
         image.clone().ok_or_else(|| anyhow!("No Image Present"))?,
@@ -75,77 +60,18 @@ impl Game for WalkTheDog {
       height: 600.0,
     });
 
-    // let current_sprite = (self.frame / 3) + 1;
-    // let frame_name = format!("Run ({}).png", current_sprite);
-    // let sprite = self
-    //   .sheet
-    //   .as_ref()
-    //   .and_then(|sheet| sheet.frames.get(&frame_name))
-    //   .expect("Cell not found");
-
-    // self.image.as_ref().map(|image| {
-    //   renderer.draw_image(
-    //     &image,
-    //     &Rect {
-    //       x: sprite.frame.x.into(),
-    //       y: sprite.frame.y.into(),
-    //       width: sprite.frame.w.into(),
-    //       height: sprite.frame.h.into(),
-    //     },
-    //     &Rect {
-    //       x: self.position.x.into(),
-    //       y: self.position.y.into(),
-    //       width: sprite.frame.w.into(),
-    //       height: sprite.frame.h.into(),
-    //     },
-    //   );
-    // });
-
     self.rhb.as_ref().unwrap().draw(renderer);
   }
 
   fn update(&mut self, keystate: &KeyState) {
-    // const SPEED_WALK: i16 = 5;
-    // let mut velocity = Point { x: 0, y: 0 };
     if keystate.is_pressed("ArrowDown") {
-      //   velocity.y += SPEED_WALK;
       self.rhb.as_mut().unwrap().slide();
     }
 
-    // if keystate.is_pressed("ArrowUp") {
-    //   velocity.y -= SPEED_WALK;
-    // }
-
     if keystate.is_pressed("ArrowRight") {
-      // velocity.x += SPEED_WALK;
       self.rhb.as_mut().unwrap().run_right();
     }
 
-    // if keystate.is_pressed("ArrowLeft") {
-    //   velocity.x -= 5;
-    // }
-
-    // if self.position.x > 600 {
-    //   self.position.x = 0;
-    // } else if self.position.x < 0 {
-    //   self.position.x = 600;
-    // } else {
-    //   self.position.x += velocity.x;
-    // }
-
-    // if self.position.y > 600 {
-    //   self.position.y = 0;
-    // } else if self.position.y < 0 {
-    //   self.position.y = 600;
-    // } else {
-    //   self.position.y += velocity.y;
-    // }
-
-    // if self.frame < 23 {
-    //   self.frame += 1;
-    // } else {
-    //   self.frame = 0;
-    // }
     self.rhb.as_mut().unwrap().update();
   }
 }
@@ -239,10 +165,18 @@ mod red_hat_boy {
     fn transition(self, event: Event) -> Self {
       match (self, event) {
         (RedHatBoyStateMachine::Idle(state), Event::Run) => state.run().into(),
-        (RedHatBoyStateMachine::Running(state), Event::Slide) => state.slide().into(),
-        (RedHatBoyStateMachine::Idle(state), Event::Update) => state.update().into(),
-        (RedHatBoyStateMachine::Running(state), Event::Update) => state.update().into(),
-        (RedHatBoyStateMachine::Sliding(state), Event::Update) => state.update().into(),
+        (RedHatBoyStateMachine::Running(state), Event::Slide) => {
+          state.slide().into()
+        }
+        (RedHatBoyStateMachine::Idle(state), Event::Update) => {
+          state.update().into()
+        }
+        (RedHatBoyStateMachine::Running(state), Event::Update) => {
+          state.update().into()
+        }
+        (RedHatBoyStateMachine::Sliding(state), Event::Update) => {
+          state.update().into()
+        }
         _ => self,
       }
     }
@@ -266,22 +200,6 @@ mod red_hat_boy {
     fn update(self) -> Self {
       self.transition(Event::Update)
     }
-    // fn update(self) -> Self {
-    //   match self {
-    //     RedHatBoyStateMachine::Idle(mut state) => {
-    //       state.update();
-    //       RedHatBoyStateMachine::Idle(state)
-    //     }
-    //     RedHatBoyStateMachine::Running(mut state) => {
-    //       state.update();
-    //       RedHatBoyStateMachine::Running(state)
-    //     }
-    //     RedHatBoyStateMachine::Sliding(mut state) => {
-    //       state.update();
-    //       RedHatBoyStateMachine::Sliding(state)
-    //     }
-    //   }
-    // }
   }
 
   impl From<RedHatBoyState<Idle>> for RedHatBoyStateMachine {
@@ -408,7 +326,6 @@ mod red_hat_boy {
       }
     }
   }
-
 
   enum SlidingEndState {
     Complete(RedHatBoyState<Running>),
